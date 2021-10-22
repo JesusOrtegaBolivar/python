b1fe7d483e72f0b0707f9b097d55d875e0cadad3
Jesus Ortega Bolviar <jesus.ortega.bolivar@gmail.com>
2021-10-22 18:04:29 +0200

    commit

--- practica menus y metodos/main.py
+++ practica menus y metodos/main.py
@@ -1,4 +1,5 @@
 from metodos import *
+import time
 opcion = int(mostrarMenu())
 
 if(opcion == 1):
@@ -7,3 +8,19 @@ elif(opcion == 2):
     narcisista()
 elif(opcion == 3):
     bisiestosnacimiento()
+elif(opcion == 0):
+    print("Saliendo del programa")
+    time.sleep(1)
+    print("...")
+    time.sleep(1)
+    print("...")
+    time.sleep(1)
+    print("...")
+else:
+    print("Ha ocurrido un error, saliendo del programa")
+    time.sleep(1)
+    print("...")
+    time.sleep(1)
+    print("...")
+    time.sleep(1)
+    print("...")
