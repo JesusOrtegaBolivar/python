6af46141eb990100a18121a5631dd6e332fb7142
Jesus Ortega Bolivar <jesus.ortega.bolivar@gmail.com>
2021-10-25 13:03:41 +0200

    commit

--- interfazGrafica/main.py
+++ interfazGrafica/main.py
@@ -1,18 +1,17 @@
 from tkinter import *
 
-
 #Definicion de funciones
 def numeroMayorYmenor():
-    ejercicio1 = Toplevel(root)
-    ejercicio1.title("New Window")
-    ejercicio1.geometry("500x500")
-    Label(ejercicio1, text ="Este ejercicio consiste en introducir 3 números para saber cual es mayor").pack()
-    Label(ejercicio1, text ="Introduce tres numeros: ").pack()
-    x1 = Entry(ejercicio1, width=50)
+    ejercicio = Toplevel(root)
+    ejercicio.title("Organizar numeros")
+    ejercicio.geometry("500x500")
+    Label(ejercicio, text ="Este ejercicio consiste en introducir 3 números para saber cual es mayor").pack()
+    Label(ejercicio, text ="Introduce tres numeros: ").pack()
+    x1 = Entry(ejercicio, width=50)
     x1.pack()
-    x2 = Entry(ejercicio1, width=50)
+    x2 = Entry(ejercicio, width=50)
     x2.pack()
-    x3 = Entry(ejercicio1, width=50)
+    x3 = Entry(ejercicio, width=50)
     x3.pack()
     def comprobar():
         numero1 = int(x1.get())
@@ -20,28 +19,57 @@ def numeroMayorYmenor():
         numero3 = int(x3.get())
         suma = numero1 + numero2 + numero3
         if(numero1 > numero2 and numero1 > numero3):
-            resultado = Label(ejercicio1, text="El numero: " + str(numero1) + " es mayor")
+            resultado = Label(ejercicio, text="El numero: " + str(numero1) + " es mayor")
             resultado.pack()
         elif(numero2 > numero1 and numero2 > numero3):
-            resultado = Label(ejercicio1, text="El numero: " + str(numero2) + " es mayor")
+            resultado = Label(ejercicio, text="El numero: " + str(numero2) + " es mayor")
             resultado.pack()
         elif(numero3 > numero1 and numero3 > numero2):
-            resultado = Label(ejercicio1, text="El numero: " + str(numero3) + " es mayor")
+            resultado = Label(ejercicio, text="El numero: " + str(numero3) + " es mayor")
             resultado.pack()
 
         if(numero1 > numero2 and numero1 < numero3 or numero1 < numero2 and numero1 > numero3):
-            resultado = Label(ejercicio1, text="El numero intermedio es: " + str(numero1) )
+            resultado = Label(ejercicio, text="El numero intermedio es: " + str(numero1) )
             resultado.pack()
         elif(numero2 > numero1 and numero2 < numero3 or numero2 < numero1 and numero1 > numero3):
-            resultado = Label(ejercicio1, text="El numero intermedio es: " + str(numero2))
+            resultado = Label(ejercicio, text="El numero intermedio es: " + str(numero2))
             resultado.pack()
         else:
-            resultado = Label(ejercicio1, text="El numero intermedio es: " + str(numero3))
+            resultado = Label(ejercicio, text="El numero intermedio es: " + str(numero3))
             resultado.pack()
-        resultado = Label(ejercicio1, text="La suma de todos los numeros es: " + str(suma))
+        resultado = Label(ejercicio, text="La suma de todos los numeros es: " + str(suma))
         resultado.pack()
-    buton = Button(ejercicio1, text="Comprobar resultado", command=comprobar)
+    buton = Button(ejercicio, text="Comprobar resultado", command=comprobar)
     buton.pack()
+
+def numerospares():
+    ejercicio = Toplevel(root)
+    ejercicio.title("Numeros pares")
+    ejercicio.geometry("500x500")
+    Label(ejercicio, text ="Este ejercicio consiste en un numero inicial y un numero final, para separarlos en pares e impares").pack()
+    Label(ejercicio, text ="Introduce el numero inicial y final: ").pack()
+    scrollbar = Scrollbar(ejercicio)
+    scrollbar.config( command = ejercicio.yview )
+    scrollbar.pack( side = RIGHT, fill = Y )
+
+    x1 = Entry(ejercicio, width=50)
+    x1.pack()
+    x2 = Entry(ejercicio, width=50)
+    x2.pack()
+    def comprobar():
+        inicial = int(x1.get())
+        final = int(x2.get())
+        for i in range(inicial, final + 1):
+            if(i % 2 == 0):
+                resultado = Label(ejercicio, text="El numero: " + str(i) + " es par")
+                resultado.pack()
+            else:
+                resultado = Label(ejercicio, text="El numero: " + str(i) + " es impar")
+                resultado.pack()
+    buton = Button(ejercicio, text="Comprobar resultado", command=comprobar)
+    buton.pack()
+
+
 
 
 
@@ -66,12 +94,12 @@ label.config(bg="#FFFFFF")
 label.pack()
 
 #Botones para los ejercicios
-button1 = Button(mainframe, text="Organizar números", command=numeroMayorYmenor)
-button1.config(width=25)
-button1.pack()
-button1 = Button(mainframe, text="Organizar números", command=numeroMayorYmenor)
-button1.config(width=25)
-button1.pack()
+button = Button(mainframe, text="Organizar números", command=numeroMayorYmenor)
+button.config(width=25)
+button.pack(pady=5)
+button = Button(mainframe, text="Numeros pares", command=numerospares)
+button.config(width=25)
+button.pack(pady = 5)
 
 
 
