5c662fa891046efea46cf713f5ebd0b4334c65b2
Jesus Ortega Bolviar <jesus.ortega.bolivar@gmail.com>
2021-11-03 16:09:41 +0100

    commit

--- sql/conexionhospital.py
+++ sql/conexionhospital.py
@@ -6,4 +6,12 @@ password = "azure"
 class ConexionHospital:
     def __init__(self):
         self.conexion = pyodbc.connect("DRIVER={ODBC Driver 17 for SQL Server};SERVER=" + servidor + "; DATABASE=" + bbdd + "; UID=" + usuario + ";PWD=" + password)
-        
+    
+    def eliminarEnfermo(self, inscripcion):
+        cursor = self.conexion.cursor()
+        sqldelete = "delete from enfermo where inscripcionn = ?"
+        cursor.execute(sqldelete, (inscripcion))
+        eliminados = cursor.rowcount
+        cursor.commit()
+        cursor.close()
+        return eliminados
